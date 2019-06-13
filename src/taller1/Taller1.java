919fb112db3241e7acb61339126a8fdf4d067b22
Andrey Herrera <kahs_kevin@hotmail.com>
2019-06-13 15:23:47 -0500

    Analizador Léxico

--- src/taller1/Taller1.java
+++ src/taller1/Taller1.java
@@ -9,13 +9,37 @@ package taller1;
  *
  * @author Andrey
  */
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Taller1 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+
+            // Asignación del nombre de archivo por defecto que usará la aplicación
+            String archivo = "C:\\Users\\Andrey\\Desktop\\Taller1\\src\\taller1\\prueba.txt";
+
+            // Se trata de leer el archivo y analizarlo en la clase que se ha creado con JFlex
+            BufferedReader buffer = new BufferedReader(new FileReader(archivo));
+            AnalizadorLexico analizadorJFlex = new AnalizadorLexico(buffer);
+
+            while (true) {
+
+                // Obtener el token analizado y mostrar su información
+                TokenPersonalizado token = analizadorJFlex.yylex();
+
+                if (!analizadorJFlex.existenTokens()) {
+                    break;
+                }
+
+                System.out.println(token.toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
     }
-    
 }
